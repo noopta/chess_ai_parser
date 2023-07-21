eb1b80f1924ea22bc865adc77bab23e32634bdcb
Anupta Islam <anuptaislam33@gmail.com>
2023-07-20 22:09:57 -0400

    adding a form

--- fe/chess_parser_fe/src/App.js
+++ fe/chess_parser_fe/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import './landingComponents/landingForm';
+// import './landingComponents/landingForm';
 import Reac, { useState, useEffect } from 'react';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 }
 
 
-const landingForm = (showComponent, setShowComponent) => {
+const LandingForm = (showComponent, setShowComponent) => {
 
   const handleSubmit = (event) => {
     event.preventDefault(); // This prevents the default behavior of form submission (page refresh)
@@ -20,6 +20,12 @@ const landingForm = (showComponent, setShowComponent) => {
 
     setShowComponent(true)
   }
+
+  const [showModel, setShowModel] = useState(false);
+
+  const handleModelToggle = () => {
+    setShowModel(!showModel);
+  };
 
   return (
     <>
@@ -51,8 +57,9 @@ const landingForm = (showComponent, setShowComponent) => {
 
         <p className="mt-10 text-center text-sm text-gray-500">
           Got a suggestion?{' '}
-          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+          <a href="#" onClick={handleModelToggle} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
             Submit any bugs here
+            {showModel && <FeedbackForm />}
           </a>
         </p>
       </div >
@@ -73,50 +80,6 @@ const Grid = () => {
     </div>
   );
 };
-
-function chessGameGrid() {
-  return (
-    <>
-      <div class="bg-white py-24 sm:py-32">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-            <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p>
-          </div>
-          <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            <article class="flex max-w-xl flex-col items-start justify-between">
-              <div class="flex items-center gap-x-4 text-xs">
-                <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
-                <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
-              </div>
-              <div class="group relative">
-                <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href="#">
-                    <span class="absolute inset-0"></span>
-                    Boost your conversion rate
-                  </a>
-                </h3>
-                <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-              </div>
-              <div class="relative mt-8 flex items-center gap-x-4">
-                <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50" />
-                <div class="text-sm leading-6">
-                  <p class="font-semibold text-gray-900">
-                    <a href="#">
-                      <span class="absolute inset-0"></span>
-                      Michael Foster
-                    </a>
-                  </p>
-                  <p class="text-gray-600">Co-Founder / CTO</p>
-                </div>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
 
 function gameCard() {
   var whiteKnight = "https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/chess_piece_knight_white.png"
@@ -175,9 +138,9 @@ const HeroSection = (props) => {
             </div>
           </nav>
           {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
-          <div class="lg:hidden" role="dialog" aria-modal="true">
-            {/* <!-- Background backdrop, show/hide based on slide-over state. --> */}
-            <div class="fixed inset-0 z-50"></div>
+          {/* <div class="lg:hidden" role="dialog" aria-modal="true"> */}
+          {/* <!-- Background backdrop, show/hide based on slide-over state. --> */}
+          {/* <div class="fixed inset-0 z-50"></div>
             <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div class="flex items-center justify-between">
                 <a href="#" class="-m-1.5 p-1.5">
@@ -204,8 +167,8 @@ const HeroSection = (props) => {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </div> */}
+          {/* </div> */}
         </header>
 
         <div class="relative isolate px-6 lg:px-8">
@@ -214,7 +177,7 @@ const HeroSection = (props) => {
               <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Chess analysis with AI, tailored for your skill set.</h1>
               <p class="mt-6 text-lg leading-8 text-gray-600">Get started by entering a Chess.com username to get feedback on your recent games.</p>
               <div class="mt-10 flex items-center justify-center gap-x-6">
-                {landingForm(showComponent, setShowComponent)}
+                {LandingForm(showComponent, setShowComponent)}
               </div>
             </div>
           </div>
@@ -222,35 +185,122 @@ const HeroSection = (props) => {
       </div>
 
       {showComponent ? <Grid /> : null}
-      {/* {isGameListVisible ? console.log("yo") : null} */}
-
-      {/* Or you can use conditional rendering with a ternary operator */}
-      {/* {isVariableTrue ? <MyFunctionalComponent /> : <div>Variable is false.</div>} */}
-      {/* {Grid()} */}
     </>
   )
 }
 
-function inputForm() {
+function FeedbackForm() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOverlayClick = (event) => {
+    // Prevent the click event from propagating to underlying elements
+    event.stopPropagation();
+  };
+
+  const handleCloseModal = () => {
+    // Add your code here to handle modal closure
+  };
+
   return (
-    <>
-      <div>
-        {/* <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Username</label> */}
-        <div class="relative mt-2 rounded-md shadow-sm">
-          {/* <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <span class="text-gray-500 sm:text-sm">$</span>
-          </div> */}
-          <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00" />
-          <div class="absolute inset-y-0 right-0 flex items-center">
-            <label for="currency" class="sr-only">Chess Platform</label>
-            <select id="currency" name="currency" class="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-              <option>Chess.com</option>
-              <option>Lichess</option>
-            </select>
+    <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" onClick={handleCloseModal}></div>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0" onClick={handleCloseModal}>
+          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg" onClick={(event) => event.stopPropagation()}>
+            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div class="sm:flex sm:items-start">
+                {/* <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                  <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                  </svg>
+                </div> */}
+                <InputForm />
+                {/* <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Feedback</h3>
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">Your critiques or compliments are important! Feel free to let us know about any improvements, things you liked, etc. below.</p>
+                  </div>
+                </div> */}
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
+  )
+}
+
+function InputForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleFormButtonClick = (event) => {
+    // Access the inputValue here and do whatever you want with it
+    event.preventDefault();
+    console.log("first name = " + firstName);
+    console.log("last name = " + lastName);
+    console.log("email = " + email);
+    console.log("message " + message);
+  };
+
+  return (
+    <div class="isolate bg-white px-6 py-24 lg:px-8">
+      <div class="mx-auto max-w-2xl text-center">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Feedback Form</h2>
+        <p class="mt-2 text-lg leading-8 text-gray-600">Your critiques and compliments matter! Feel free to fill out the following form and let us know what you loved, or what we can improve on.</p>
+      </div>
+      <form class="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleFormButtonClick}>
+        <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+            <div class="mt-2.5">
+              <input type="text" value={firstName} onChange={handleFirstNameChange} name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div>
+            <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+            <div class="mt-2.5">
+              <input type="text" value={lastName} onChange={handleLastNameChange} name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div class="sm:col-span-2">
+            <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+            <div class="mt-2.5">
+              <input type="email" value={email} onChange={handleEmailChange} name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div class="sm:col-span-2">
+            <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
+            <div class="mt-2.5">
+              <textarea name="message" value={message} onChange={handleMessageChange} id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            </div>
+          </div>
+        </div>
+        <div class="mt-10">
+          <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send message</button>
+        </div>
+      </form>
+    </div>
+
   )
 }
 
