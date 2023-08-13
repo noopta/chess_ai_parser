c52610a9aecf183868fbb7139fb9e606dbdb44fe
Anupta Islam <anuptaislam33@gmail.com>
2023-08-13 03:41:59 -0400

    adding loading spinner for Lambda call

--- fe/chess_parser_fe/src/App.js
+++ fe/chess_parser_fe/src/App.js
@@ -12,6 +12,18 @@ function App() {
   );
 }
 
+const Spinner = () => {
+  return (
+    <div role="status">
+      <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
+  )
+}
+
 const LandingForm = (showComponent, setShowComponent) => {
 
   const handleSubmit = (event) => {
@@ -31,6 +43,8 @@ const LandingForm = (showComponent, setShowComponent) => {
     setShowModel(!showModel);
   };
 
+
+
   return (
     <>
       <div className=" sm:mx-auto sm:w-full sm:max-w-sm">
@@ -39,6 +53,8 @@ const LandingForm = (showComponent, setShowComponent) => {
             <label className="block text-sm font-medium leading-6 text-gray-900">
               Username
             </label>
+            {/* show spinner if isLoading is true */}
+            {isLoading && <LoadingModel />}
             <div className="mt-2">
               <input
                 id="text"
@@ -111,7 +127,75 @@ function extractJSONFromBody2(bodyString) {
   }
 }
 
-const MakePostRequest = async ({ setIsLoading, setData }) => {
+const CubeSpinner = () => {
+  return (
+    <main>
+      <svg class="ip" viewBox="0 0 256 128" width="256px" height="128px" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <linearGradient id="grad1" x1="0" y1="0" x2="1" y2="0">
+            <stop offset="0%" stop-color="#5ebd3e" />
+            <stop offset="33%" stop-color="#ffb900" />
+            <stop offset="67%" stop-color="#f78200" />
+            <stop offset="100%" stop-color="#e23838" />
+          </linearGradient>
+          <linearGradient id="grad2" x1="1" y1="0" x2="0" y2="0">
+            <stop offset="0%" stop-color="#e23838" />
+            <stop offset="33%" stop-color="#973999" />
+            <stop offset="67%" stop-color="#009cdf" />
+            <stop offset="100%" stop-color="#5ebd3e" />
+          </linearGradient>
+        </defs>
+        <g fill="none" stroke-linecap="round" stroke-width="16">
+          <g class="ip__track" stroke="#ddd">
+            <path d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
+            <path d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
+          </g>
+          <g stroke-dasharray="180 656">
+            <path class="ip__worm1" stroke="url(#grad1)" stroke-dashoffset="0" d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
+            <path class="ip__worm2" stroke="url(#grad2)" stroke-dashoffset="358" d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
+          </g>
+        </g>
+      </svg>
+    </main>
+  )
+}
+
+const LoadingModel = () => {
+  return (
+    <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div class="sm:flex sm:items-start">
+                {/* <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                  <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                </svg>
+                </div> */}
+                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-centre">
+                  <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Loading Games</h3>
+                  <div class="mt-2 text-centre content-centre">
+                    {/* <Spinner /> */}
+                    <CubeSpinner />
+                    <p class="text-sm text-gray-500">Hold on! We are working our hardest to analyze your games, after all good things take time :p</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
+              <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+const MakePostRequest = async ({ isLoading, setIsLoading, setData }) => {
 
   // useEffect(() => {
   // const fetchData = async () => {
@@ -121,7 +205,8 @@ const MakePostRequest = async ({ setIsLoading, setData }) => {
   const url = 'https://4vz6fr5rinuhsspb6rh5alkige0jrcmo.lambda-url.us-east-2.on.aws/'
   try {
     console.log("here")
-    // setIsLoading(true);
+    setIsLoading(true);
+    console.log(isLoading)
     console.log("before fetch")
     const response = await fetch(url, {
       method: 'POST',
@@ -149,6 +234,7 @@ const MakePostRequest = async ({ setIsLoading, setData }) => {
 
     // var jsonBody = extractJSONFromBody(extractJSONFromBody2(responseData))
     var jsonBody = JSON.parse(responseData)
+
     // var jsonBody = extractJSONFromBody2(responseData)
     // var temp = extractJSONFromBody2(responseData)
     for (var i = 0; i < jsonBody.length; i++) {
@@ -196,14 +282,8 @@ const MakePostRequest = async ({ setIsLoading, setData }) => {
 
 const GetGames = async (showComponent, setShowComponent, isLoading, setIsLoading, data, setData) => {
   console.log("get games")
-
-  setIsLoading(true)
-
   try {
-    var requestResponse = await MakePostRequest({ setIsLoading, setData })
-
-    console.log(globalGames[0])
-    // console.log(requestResponse)
+    var requestResponse = await MakePostRequest({ isLoading, setIsLoading, setData })
   } catch {
     console.log("error")
   } finally {
@@ -213,28 +293,16 @@ const GetGames = async (showComponent, setShowComponent, isLoading, setIsLoading
     }
   }
 
-  setIsLoading(false)
-
-
-  const Spinner = () => {
-    return (
-      <div role="status">
-        <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-        </svg>
-        <span class="sr-only">Loading...</span>
-      </div>
-    )
-  }
-
 
 
   // MakePostRequest()
 
-  if (isLoading) return (
-    <Spinner />
-  )
+  // if (isLoading) {
+  //   console.log("spinning")
+  //   return (
+  //     <Spinner />
+  //   )
+  // }
 
   if (!data) return (
     <span>Data not available</span>
